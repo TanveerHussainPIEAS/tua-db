bb71ce0f449dc6cdab417ab782d0b080c6b7b0a5
Tanveer Hussain <thussain@cinnova.com>
2023-02-02 03:10:29 +0500

    ...

---
+++ TUA_DB/Tables/Country.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Country] (
+    [Id]                    INT             IDENTITY (1, 1) NOT NULL,
+    [Name]                  NVARCHAR (50)   NOT NULL,
+    [CountryCode]           NVARCHAR (50)   NULL,
+    [Lat]                   VARCHAR (500)   NULL,
+    [Lang]                  VARCHAR (500)   NULL,
+    [Deleted]               BIT             CONSTRAINT [DF_Deleted] DEFAULT ((0))   NOT NULL,
+    CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED ([Id] ASC),
+)
