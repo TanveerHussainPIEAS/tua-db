191ac31f14cf199fffe4ec89916174c7df65c45c
Tanveer Hussain <thussain@cinnova.com>
2024-02-16 01:10:44 +0500

    .............designers

---
+++ TUA_DB/Tables/ProductDesigners.sql
@@ -0,0 +1,17 @@
+﻿
+
+CREATE TABLE [dbo].[ProductDesigners]
+(
+	[Id]					INT				IDENTITY(1,1) NOT NULL,	
+	[Name]					NVARCHAR(300)	NOT NULL,
+	[Detail]				NVARCHAR(900)	NULL,
+	[Deleted]				BIT				CONSTRAINT [DF_ProductDesigners_IsDeleted] DEFAULT (0) NULL,
+	[CreatedDate]			DATETIMEOFFSET	CONSTRAINT [DF_ProductDesigners_Created] DEFAULT (GETUTCDATE()) NULL,	
+	[ModifiedDate]			DATETIMEOFFSET 	CONSTRAINT [DF_ProductDesigners_Modified] DEFAULT (GETUTCDATE()) NULL,
+	[DeletedDate]			DATETIMEOFFSET	NULL,
+    [DeletedBy]				INT   NULL,
+    [ModifiedBy]			INT   NULL,
+	[CreatedBy]				INT   NULL,
+	CONSTRAINT [PK_ProductDesigners] PRIMARY KEY CLUSTERED ([ID] ASC),
+)
+
