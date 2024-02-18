8d8c9a5d1f0b92fd177d2bc80b333cd8434eee9a
Tanveer Hussain <thussain@cinnova.com>
2024-02-18 23:35:50 +0500

    ...

--- TUA_DB/Tables/ProductEdits.sql
+++ TUA_DB/Tables/ProductEdits.sql
@@ -5,6 +5,7 @@ CREATE TABLE [dbo].[ProductEdits]
 	[Id]					INT				IDENTITY(1,1) NOT NULL,	
 	[Name]					NVARCHAR(300)	NOT NULL,
 	[Detail]				NVARCHAR(900)	NULL,
+	[ImageUrl]				NVARCHAR(900)	NULL,	
 	[Deleted]				BIT				CONSTRAINT [DF_ProductEdits_IsDeleted] DEFAULT (0) NULL,
 	[CreatedDate]			DATETIMEOFFSET	CONSTRAINT [DF_ProductEdits_Created] DEFAULT (GETUTCDATE()) NULL,	
 	[ModifiedDate]			DATETIMEOFFSET 	CONSTRAINT [DF_ProductEdits_Modified] DEFAULT (GETUTCDATE()) NULL,
