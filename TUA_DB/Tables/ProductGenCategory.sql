ff6318357db0e9a555b5977fc5efa6270436a122
Tanveer Hussain <thussain@cinnova.com>
2024-02-26 14:34:02 +0500

    | Product Gen Cat | TH |

---
+++ TUA_DB/Tables/ProductGenCategory.sql
@@ -0,0 +1,23 @@
+﻿
+
+CREATE TABLE [dbo].[ProductGenCategory]
+(
+	[Id]					INT				IDENTITY(1,1) NOT NULL,	
+	[Type]					NVARCHAR(300)	NOT NULL,
+	[Deleted]				BIT				CONSTRAINT [DF_ProductGenCategory_IsDeleted] DEFAULT (0) NOT NULL,
+	[CreatedDate]			DATETIMEOFFSET	CONSTRAINT [DF_ProductGenCategory_Created] DEFAULT (GETUTCDATE()) NOT NULL,	
+	[ModifiedDate]			DATETIMEOFFSET 	CONSTRAINT [DF_ProductGenCategory_Modified] DEFAULT (GETUTCDATE()) NOT NULL,
+	[DeletedDate]			DATETIMEOFFSET	NULL,
+    [DeletedBy]				INT   NULL,
+    [ModifiedBy]			INT   NULL,
+	[CreatedBy]				INT   NULL,
+	CONSTRAINT [PK_ProductGenCategory] PRIMARY KEY CLUSTERED ([ID] ASC),
+	--CONSTRAINT [FK_Activity_ActivityStatus] FOREIGN KEY ([StatusId]) REFERENCES [dbo].[ActivityStatus] ([ID]),
+	--CONSTRAINT [FK_Activity_Currency] FOREIGN KEY ([CurrencyId]) REFERENCES [dbo].[Currency] ([ID]),
+	--CONSTRAINT [FK_Activity_Country] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[Country] ([ID]),
+	--CONSTRAINT [FK_Activity_DeletedBy_User] FOREIGN KEY ([DeletedBy]) REFERENCES [dbo].[Users] ([Id]),
+ --   CONSTRAINT [FK_Activity_CreatedBy_User] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
+ --   CONSTRAINT [FK_Activity_ModifiedBy_User] FOREIGN KEY ([ModifiedBy]) REFERENCES [dbo].[Users] ([Id]),
+	--CONSTRAINT [FK_Activity_ActivityType] FOREIGN KEY ([TypeId]) REFERENCES [dbo].[ActivityType] ([Id]),
+)
+
